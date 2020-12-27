241ff06a7d063583f58c68730780d955b9565c9f
chi-moua <kongfuechimoua@gmail.com>
2020-12-27 14:43:30 -0600

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Kongfuechi Moua on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
